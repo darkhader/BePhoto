d9c79ffeff7ef2cf74299f4a70e1cd624b97bb5d
unknown <dr.hiep98@gmail.com>
2019-12-24 13:06:46 +0700

    B

---
+++ src/main/java/com/ltu/photoeditor/Keys.java
@@ -0,0 +1,8 @@
+package com.ltu.photoeditor;
+
+public class Keys {
+    public static final String CREATIVE_SDK_CLIENT_ID = "6f14db9074124260ad34a90a7f46dfbb";
+    public static final String CREATIVE_SDK_CLIENT_SECRET = "363df5d3-9cdb-46d6-93b0-3bb11dae52e7";
+    public static final String CREATIVE_SDK_REDIRECT_URI = "ams+284237f50180655cdc906d6651b9aa7953138b45://adobeid/6f14db9074124260ad34a90a7f46dfbb";
+    public static final String[] CREATIVE_SDK_SCOPES = { "email", "profile", "address" };
+}
---
+++ src/main/java/com/ltu/photoeditor/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.ltu.photoeditor;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/** A dummy splash screen activity. */
+public class SplashActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+
+    int delayDuration = 1500;
+    new Handler().postDelayed(() -> {
+      startActivity(HomeActivity.getIntent(SplashActivity.this));
+      finish();
+    }, delayDuration);
+  }
+}
